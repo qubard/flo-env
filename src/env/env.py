6eaaea677675ebc1bcf74c9b20b1d91c69db42fb
qubard <qubard@gmail.com>
2019-01-14 22:47:31 -0500

    handling quit

--- src/env/env.py
+++ src/env/env.py
@@ -1,17 +1,26 @@
 import pygame
-import time
 
 
 class Environment:
     def __init__(self, dimensions=(1024,768)):
         self.dimensions = dimensions
         self.screen = None
+        self.shouldRun = True
+
+    def handle_events(self):
+        for event in pygame.event.get():
+            self.handle_event(event)
+
+    def handle_event(self, event):
+        if event.type == pygame.QUIT:
+            self.shouldRun = False
 
     def run(self):
         pygame.display.init()
         self.screen = pygame.display.set_mode(self.dimensions)
-        while True:
+        while self.shouldRun:
+            self.handle_events()
             pygame.draw.rect(self.screen, (255, 255, 255), pygame.Rect(0, 0, 50, 50))
 
             pygame.display.flip()
-            time.sleep(1)
+            pygame.time.wait(1)
