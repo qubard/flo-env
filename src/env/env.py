297f864d57e950a7eb2fd44b41a74c16409e4a3e
qubard <qubard@gmail.com>
2019-01-24 08:31:09 -0500

    rendering relative to agent

--- src/env/env.py
+++ src/env/env.py
@@ -1,19 +1,22 @@
 import pygame, random
-from src.entity import Entity
+from src.env.entity import Entity
 
 from hashlib import md5
 
 from math import radians, cos, sin
 
 class Environment:
-    def __init__(self, dimensions=(500, 500), render=False, keyboard=False, seed=0):
-        self.dimensions = dimensions
+    def __init__(self, dimensions=(50, 50), scale=1, render=False, keyboard=False, seed=0):
+        self.dimensions = (dimensions[0] * scale, dimensions[1] * scale)
+
+        self.scale = scale
+
         self.render = render
         self.keyboard = keyboard
         self.screen = None
         self.shouldRun = True
 
-        self.player = Entity(x=dimensions[0] / 2, y=dimensions[1] / 2, size=25)
+        self.player = Entity(x=self.dimensions[0] / 2, y=self.dimensions[1] / 2, size=scale)
         self.left = False
         self.right = False
         self.up = False
@@ -23,10 +26,12 @@ class Environment:
 
         random.seed(seed)
 
-        self.spawn_segments = [ ((0, -self.player.size), (dimensions[0] - self.player.size, -self.player.size), (0, 180)),\
-                                ((-self.player.size, 0), (-self.player.size, dimensions[1] - self.player.size), (-90, 90)), \
-                                ((0, dimensions[1] + self.player.size), (dimensions[0] - self.player.size, dimensions[1] + self.player.size), (180, 360)), \
-                                ((dimensions[0] + self.player.size, 0), (dimensions[0] + self.player.size, dimensions[1] - self.player.size), (90, 180)) ]
+        self.spawn_segments = [ ((0, -self.player.size), (self.dimensions[0] - self.player.size, -self.player.size), (0, 180)),\
+                                ((-self.player.size, 0), (-self.player.size, self.dimensions[1] - self.player.size), (-90, 90)), \
+                                ((0, self.dimensions[1] + self.player.size), \
+                                 (self.dimensions[0] - self.player.size, self.dimensions[1] + self.player.size), (180, 360)), \
+                                ((self.dimensions[0] + self.player.size, 0), (self.dimensions[0] + self.player.size, \
+                                                                              self.dimensions[1] - self.player.size), (90, 180)) ]
 
         self.valid_keys = { pygame.K_LEFT : 'left', pygame.K_RIGHT: 'right', pygame.K_DOWN: 'down', pygame.K_UP: 'up' }
 
@@ -66,18 +71,22 @@ class Environment:
 
     # Spawn an entity
     def _spawn_projectile(self):
-        if len(self.projectiles) < 100:
+        if len(self.projectiles) < 200:
             segment = self.spawn_segments[random.randint(0, 3)]
             pos = (random.uniform(segment[0][0], segment[1][0]), random.uniform(segment[0][1], segment[1][1]))
             angle = radians(random.randint(segment[2][0], segment[2][1]))
             dir = (cos(angle), sin(angle))
-            self.projectiles.append(Entity(x=pos[0], y=pos[1], size=25, vx=dir[0], vy=dir[1]))
+            self.projectiles.append(Entity(x=pos[0], y=pos[1], size=self.scale, vx=dir[0], vy=dir[1]))
 
+    def render_entity(self, entity):
+        if self.player:
+            pygame.draw.rect(self.background, (0, 0, 0),
+                         (entity.position[0] - self.player.x + self.dimensions[0] / 2,
+                          entity.position[1] - self.player.y + self.dimensions[1] / 2, entity.size, entity.size))
 
     def _render_projectiles(self):
         for entity in self.projectiles:
-            pygame.draw.rect(self.background, (0, 0, 0),
-                             (entity.position[0], entity.position[1], entity.size, entity.size))
+            self.render_entity(entity)
 
     def _move_projectiles(self):
         to_remove = []
@@ -112,15 +121,15 @@ class Environment:
 
             if self.player:
                 self.handle_player_movement()
-                pygame.draw.rect(self.background, (255, 0, 0), (self.player.position[0], self.player.position[1], self.player.size, self.player.size))
+                pygame.draw.rect(self.background, (255, 0, 0), (self.dimensions[0] / 2, self.dimensions[1] / 2, \
+                                                                self.player.size, self.player.size))
 
             self._move_projectiles()
             self._render_projectiles()
 
             self._spawn_projectile()
 
-            print(self.state_hash())
-
             if self.render:
                 pygame.display.flip()
+
             pygame.time.wait(25)
